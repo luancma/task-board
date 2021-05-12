fb2a7dc6de28155f5fa5d720b8c46829c71fa656
Luan <luancmadev@gmail.com>
2021-05-12 01:45:50 -0300

    created the modal

--- src/components/CreateLabel/index.jsx
+++ src/components/CreateLabel/index.jsx
@@ -5,16 +5,9 @@ import { uuid } from "uuidv4";
 import { api } from "../../services/api";
 
 export function CreateLabel({ open, closeModal, cards, id, name }) {
-  const { tasks, handleSetTasks } = useContext(TaskContext);
+  const { tasks } = useContext(TaskContext);
   const [value, setValue] = useState("");
   const createTask = () => {
-    const prevList = {
-      id: uuid(),
-      status: id,
-      title: `Title task ${Math.random()}`,
-      description: `New task ${Math.random()}`,
-    };
-
     const clonedTasks = tasks.filter((task) => task.id === id)[0];
 
     clonedTasks.cards.splice(1, 0, {
@@ -30,14 +23,28 @@ export function CreateLabel({ open, closeModal, cards, id, name }) {
   return (
     <>
       {!!open && (
-        <Box>
-          <Layer onEsc={closeModal} onClickOutside={closeModal}>
-            <TextInput
-              placeholder="Título"
-              value={value}
-              onChange={(event) => setValue(event.target.value)}
-            />
-            <Button label="Criar" onClick={createTask} />
+        <Box style={{ position: "absolute" }}>
+          <Layer pad="medium" onEsc={closeModal} onClickOutside={closeModal}>
+            <Box
+              width="medium"
+              height="medium"
+              justify="center"
+              align="center"
+              margin="small"
+              gap="medium"
+            >
+              <TextInput
+                placeholder="Título"
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+              />
+              <TextInput
+                placeholder="Descrição"
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+              />
+              <Button label="Criar" onClick={createTask} />
+            </Box>
           </Layer>
         </Box>
       )}
