36ebf03b160e30b0bdc1d4850655ba5c5a337cec
Luan <luancmadev@gmail.com>
2021-05-12 15:29:06 -0300

    changed the url

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import { QueryClientProvider } from "react-query";
 import "./App.css";
 import { Board } from "./components/Board";
 import { TaskStorePrivder } from "./Context";
-import { queryClient } from "./services/queryClient";
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TaskStorePrivder>
-        <Board />
-      </TaskStorePrivder>
-    </QueryClientProvider>
+    <TaskStorePrivder>
+      <Board />
+    </TaskStorePrivder>
   );
 }
 
